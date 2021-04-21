5632a6f656fadbc5017ebc31af54f91ddd69d381
jonas18121 <jonasxtrm971@gmail.com>
2021-04-21 11:14:40 +0200

    exemple geolocalisation

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -4,6 +4,10 @@ import React, { Component } from 'react';
 
 import {StyleSheet} from 'react-native';
 
+import Geolocation from 'react-native-geolocation-service';
+
+import projector from 'ecef-projector';
+
 import {
   ViroARScene,
   ViroText,
@@ -36,26 +40,202 @@ export default class HelloWorldSceneAR extends Component {
     super();
 
     // Set initial state here
-     this.state = {
-      text : "Initializing AR... ok",
-      videoPaused: true,
-      videoIndex: 0,
-      videos : [
-        {element : './video/wOof.mp4'}
-      ]
-    }; 
+    this.state = {
+        text : "Initializing AR... ok",
+        videoPaused: true,
+        videoIndex: 0,
+        videos : [
+            {element : './video/wOof.mp4'}
+        ],
+        geolocation : {
+            latitude: null,
+            longitude: null,
+            error: null,
+            selisih: null,
+            xSel: null,
+            ySel: null,
+            zSel: null,
+            distance: null,
+            string: '',
+            isString: true,
+            count: 0,
+            _condition: false,
+            condition: ''
+        } 
+    };
+    
 
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
     this._onPlayVideo = this._onPlayVideo.bind(this);
   }
 
-  
+    getCoordinate = () => {
+
+        const userxyz = projector.project(this.state.geolocation.latitude, this.state.geolocation.longitude, 0.0);
+        const objectxyz = projector.project(-6.260719, 106.781616, 0.0)
+
+        if (this.state.geolocation.count === 0) {
+            this.selisih(userxyz, objectxyz)
+        }
+        const distance = this.getDistance(this.state.geolocation.latitude, this.state.geolocation.longitude, -6.260719, 106.781616)
+        this.setState({
+            geolocation: {
+                distance: distance
+            }
+        })
+    }
+
+    selisih = (user, object) => {
+
+        let selisih = {
+            x: user[0] - object[0],
+            y: user[1] - object[1],
+            z: user[2] - object[2]
+        }
+        this.setState({
+            geolocation: {
+                xSel: selisih.x,
+                ySel: selisih.y,
+                zSel: selisih.z,
+                count: this.state.geolocation.count + 1
+            }
+        })
+    }
+
+
+    getDistance = (lon1, lat1, lon2, lat2) => {
+        if (typeof(Number.prototype.toRad) === "undefined") {
+            Number.prototype.toRad = function() {
+                return this * Math.PI / 180;
+            }
+        }
+
+        var R = 6371; // Radius of the earth in km
+
+        var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+
+        var dLon = (lon2-lon1).toRad(); // distance longitude
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+                Math.sin(dLon/2) * Math.sin(dLon/2);
+
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        var d = R * c * 1000 // Distance in km
+
+        return d.toFixed(2)
+    }
+
+    componentDidMount() {
+        Geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    geolocation: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        error: null,
+                        string: String(position.coords.latitude)
+                    }
+                });
+                this.getCoordinate()
+            },
+            (error) => this.setState({ geolocation: { error: error.message} }),
+            { enableHighAccuracy: true, timeout: 2000, maximumAge: 2000, distanceFilter: 1 },
+        )
+
+        Geolocation.watchPosition(
+            (position) => {
+                this.setState({
+                    geolocation: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        error: null,
+                        string: String(position.coords.latitude)
+                    }
+                });
+                this.getCoordinate()
+            },
+            (error) => this.setState({ geolocation: { error: error.message} }),
+            { enableHighAccuracy: true, timeout: 2000, maximumAge: 2000, distanceFilter: 1 },
+        )
+    }
+
+    _reload = () => {
+        if (this.state.geolocation.distance < 2) {
+            this.setState({
+                geolocation: {
+                    condition: 'Salam 3 jari enak',
+                    _condition: true
+                }
+            })
+        } else {
+            this.setState({
+                geolocation: {
+                    condition: 'you are not close enough',
+                    _condition: true
+                }
+            })
+        }
+    }
+
+
 
   render() {
+      const numx = this.state.geolocation.xSel;
+      const numy = this.state.geolocation.ySel;
+      const numz = this.state.geolocation.zSel;
+
       return (
+
           <ViroARScene onTrackingUpdated={this._onInitialized} >
 
+
+            {/*  Début essaie geolocalisation */}
+
+            {
+                this.state.geolocation.xSel && this.state.geolocation.ySel && this.state.geolocation.zSel &&
+
+                <ViroBox 
+                    position={[numx, 0, numz]} 
+                    scale={[.3, .3, .1]} 
+                    materials={["grid"]} 
+                    animation={{ name: "rotate", run: true, loop: true}}
+                    onClick={this._reload}
+                />
+            }
+            {
+                this.state.geolocation.distance &&
+
+                <ViroText 
+                    text={this.state.text} 
+                    scale={[3, 3, 3]} 
+                    position={[numx, 0, numz]} 
+                    style={styles.helloWorldTextStyle} 
+                />
+            }
+            {
+                this.state.geolocation._condition &&
+
+                <ViroText 
+                    text={this.state.geolocation.condition} 
+                    scale={[1, 1, 1]} 
+                    position={[numx, 0, numz]} 
+                    style={styles.helloWorldTextStyle} 
+                />
+            }
+
+            {/*  Fin essaie geolocalisation */}
+
+
+
+
+
+
+
+
+            
               {/* <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} /> */}
 
               <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} animation={{ name: "rotate", run: true, loop: true}} />
@@ -140,6 +320,8 @@ export default class HelloWorldSceneAR extends Component {
           </ViroARScene>
       );
   }
+
+    
 
   _onPlayVideo(){
 
